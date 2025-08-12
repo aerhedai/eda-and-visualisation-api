d5be3ba1183370c962e1dd123d0dca51d0746997
rohan5046 <rohanp@live.co.uk>
2025-08-12 20:56:49 +0100

    Fix issues with schema causing validation errors in pipeline testing

--- app/models/eda_schema.py
+++ app/models/eda_schema.py
@@ -1,8 +1,13 @@
 from pydantic import BaseModel
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Optional
 
+
+# -------- EDA --------
 class EDARequest(BaseModel):
-    rows: List[Dict[str, Any]]
+    dataset_id: Optional[str] = None
+    rows: Optional[List[Dict[str, Any]]] = None
+    target_column: Optional[str] = None
+
 
 class EDAResponse(BaseModel):
     summary: Dict[str, Any]
--- app/models/vis_schema.py
+++ app/models/vis_schema.py
@@ -1,11 +1,14 @@
 from pydantic import BaseModel
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Optional
 
+# -------- Visualisation --------
 class VisualisationRequest(BaseModel):
-    rows: List[Dict[str, Any]]
+    dataset_id: Optional[str] = None
+    rows: Optional[List[Dict[str, Any]]] = None
     chart_type: str  # e.g., "histogram", "scatter"
     x: str
-    y: str = None
+    y: Optional[str] = None
+
 
 class VisualisationResponse(BaseModel):
     image_base64: str  # encoded plot image
